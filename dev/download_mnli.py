117678b3622893e9a0914635134ed4a1fa9110db
clover3 <lesterny@gmail.com>
2021-09-03 17:12:33 -0400

    download bert

--- dev/download_mnli.py
+++ dev/download_mnli.py
@@ -19,7 +19,7 @@ if sys.version_info >= (3, 0):
     URLLIB=urllib.request
 
 
-def download_and_extract(data_dir):
+def download_and_extract_mnli(data_dir):
     url = 'https://dl.fbaipublicfiles.com/glue/data/MNLI.zip'
     print("Downloading and extracting MNLI dataset...")
     zip_save_path = os.path.join(data_dir, "mnli.zip")
@@ -27,11 +27,20 @@ def download_and_extract(data_dir):
     with zipfile.ZipFile(zip_save_path) as zip_ref:
         zip_ref.extractall(data_dir)
     os.remove(zip_save_path)
+    
+
+def download_and_extract_bert(data_dir):
+    url = "https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-12_H-768_A-12.zip"
+    print("Downloading and extracting BERT checkpoint...")
+    zip_save_path = os.path.join(data_dir, "bert.zip")
+    URLLIB.urlretrieve(url, zip_save_path)
+    with zipfile.ZipFile(zip_save_path) as zip_ref:
+        zip_ref.extractall(data_dir)
 
 
 def main():
     exist_or_mkdir(data_path)
-    download_and_extract(data_path)
+    download_and_extract_bert(data_path)
 
 
 if __name__ == '__main__':
--- path_manager.py
+++ path_manager.py
@@ -7,4 +7,4 @@ data_path = pjoin(project_root, 'data')
 bert_voca_path = pjoin(data_path, "bert_voca.txt")
 mnli_dir = pjoin(data_path, "MNLI")
 mnli_ex_dir = pjoin(data_path, "mnli_ex")
-bert_model_folder = NotImplemented
+bert_model_folder = pjoin(data_path, "uncased_L-12_H-768_A-12")
