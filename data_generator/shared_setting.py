7c7a3b7f12e04976c15b0020dbc8bbb28730ede1
clover3 <lesterny@gmail.com>
2021-09-03 16:04:07 -0400

    Remove unused files

--- data_generator/shared_setting.py
+++
@@ -1,8 +0,0 @@
-
-
-class BertNLI:
-    vocab_filename = "bert_voca.txt"
-    vocab_size = 30522
-    seq_length = 300
-
-
--- explain/runner/eval_snli_accuracy.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from explain.runner.eval_accuracy import eval_original_task
-
-if __name__  == "__main__":
-    eval_original_task(sys.argv[1], sys.argv[2], "snli")
