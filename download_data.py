11116dad305c5a49e35088a62c85b88b373ba2a1
clover3 <lesterny@gmail.com>
2021-09-03 17:29:55 -0400

    organize path

--- download_data.py
+++ download_data.py
@@ -21,6 +21,9 @@ if sys.version_info >= (3, 0):
 
 def download_and_extract_mnli(data_dir):
     url = 'https://dl.fbaipublicfiles.com/glue/data/MNLI.zip'
+    if os.path.exists(os.path.join(data_dir, "MNLI")):
+        print("MNLI folder found. Skip download")
+        return 
     print("Downloading and extracting MNLI dataset...")
     zip_save_path = os.path.join(data_dir, "mnli.zip")
     URLLIB.urlretrieve(url, zip_save_path)
@@ -31,6 +34,9 @@ def download_and_extract_mnli(data_dir):
 
 def download_and_extract_bert(data_dir):
     url = "https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-12_H-768_A-12.zip"
+    if os.path.exists(os.path.join(data_dir, "bert_model.ckpt.index")):
+        print("BERT model found. Skip download")
+        return 
     print("Downloading and extracting BERT checkpoint...")
     zip_save_path = os.path.join(data_dir, "bert.zip")
     URLLIB.urlretrieve(url, zip_save_path)
@@ -40,6 +46,7 @@ def download_and_extract_bert(data_dir):
 
 def main():
     exist_or_mkdir(data_path)
+    download_and_extract_mnli(data_path)
     download_and_extract_bert(data_path)
 
 
--- path_manager.py
+++ path_manager.py
@@ -4,7 +4,7 @@ from os.path import dirname
 pjoin = os.path.join
 project_root = os.path.abspath(dirname((os.path.abspath(__file__))))
 data_path = pjoin(project_root, 'data')
-bert_voca_path = pjoin(data_path, "bert_voca.txt")
+bert_voca_path = pjoin(data_path, "vocab.txt")
 mnli_dir = pjoin(data_path, "MNLI")
 mnli_ex_dir = pjoin(data_path, "mnli_ex")
-bert_model_folder = pjoin(data_path, "uncased_L-12_H-768_A-12")
+bert_model_folder = data_path
